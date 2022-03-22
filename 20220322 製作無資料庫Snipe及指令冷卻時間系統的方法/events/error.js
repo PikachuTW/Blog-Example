35a847f7d514ffc63e3ce6e32afdf0d1d2e6a468
Tails <82265690+PikachuTW@users.noreply.github.com>
2022-03-22 22:30:44 +0800

    20220322 Update

--- 20220322 製作無資料庫Snipe及指令冷卻時間系統的方法/events/error.js
+++
@@ -1,4 +0,0 @@
-const logger = require('../modules/logger.js');
-module.exports = async (client, error) => {
-    logger.error(`${JSON.stringify(error)}`);
-};
---
+++ 20220322 製作無資料庫Snipe指令的方法/events/error.js
@@ -0,0 +1,4 @@
+const logger = require('../modules/logger.js');
+module.exports = async (client, error) => {
+    logger.error(`${JSON.stringify(error)}`);
+};
